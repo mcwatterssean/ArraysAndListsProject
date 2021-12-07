0564e3e19ed16aa7de99f4c6b241c7e2fd0d444a
Sean McWatters <mcwatterssean@gmail.com>
2021-12-07 07:30:23 -0500

    init

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,32 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //TODO:
+        {  
+            var numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };           
+                var evens = new List<int>();
+                var odds = new List<int>();
 
-            // Create an int Array and populate numbers 1-10
+            /*for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evens.Add(numbers[i]);
+                }
+                else
+                {
+                    odds.Add(numbers[i]);
+                }
+            }
+            */
+            
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                    //Console.WriteLine($"{num} is an even number");
+                }
+                else
+                {
+                    odds.Add(num);
+                    //Console.WriteLine($"{num} is an odd number");
+                }
+            }
+            
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"{item} is an even number");
+            }
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
-
-            /* Now using foeach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+            foreach (var item in odds)
+            {
+                Console.WriteLine($"{item} is an odd number");
+            }
         }
     }
 }
